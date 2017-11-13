d7291cd4b04976d4bb43dda72dc571cbdcd1d39d
Clément BARBASTE <barbaste.clement@gmail.com>
2017-11-13 17:05:59 +0100

    2eme commit initial

---
+++ webpackage.config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "bundle.js"
+    },
+    plugins: [
+        new UglifyJSPlugin(),
+        new HtmlWebpackPlugin()
+    ]
+}
